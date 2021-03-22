ecadd99e322e09a06f1a704a09abb8446c27bce6
Arasia <rlacndtjs261@gmail.com>
2021-03-23 01:12:08 +0900

    Update cache-list.js

--- assets/js/data/cache-list.js
+++ assets/js/data/cache-list.js
@@ -51,6 +51,7 @@ const include = [
 
   '{{ "/assets/js/data/search.json" | relative_url }}',
   '{{ "/404.html" | relative_url }}',
+  '{{ "/index.html" | relative_url }}',
 
   '{{ "/app.js" | relative_url }}',
   '{{ "/sw.js" | relative_url }}'
