05f6b54d1413f5d6cb346a2108798d590592a1f8
Arasia <rlacndtjs261@gmail.com>
2021-03-23 23:02:57 +0900

    Remove Log

--- app.js
+++ app.js
@@ -4,10 +4,5 @@ layout: compress
 
 /* Registering Service Worker */
 if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('{{ "/sw.js" | relative_url }}').then(
-    (registration) => {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, (err) => {
-      console.log('ServiceWorker registration failed: ', err);
-  });
+  navigator.serviceWorker.register('{{ "/sw.js" | relative_url }}');
 };
